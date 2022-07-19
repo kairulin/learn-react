de44dfa13a0b8163526bc83ff331b3789aca48c4
LinKaiRu <10746041@ntub.edu.tw>
2022-07-19 15:44:36 +0800

    useEffect

--- src/Todo.js
+++ src/Todo.js
@@ -1,10 +1,15 @@
 import React from 'react'
 
-export default function ({ todo }) {
+export default function ({ todo,toggleTodo }) {
+
+    function handleTodoClick(){
+        toggleTodo(todo.id)
+    }
+
     return (
         <div>
             <label>
-            {/* <input type="checkbox" checked={todo.complete}></input> */}
+            <input type="checkbox" defaultChecked={todo.complete} onChange={handleTodoClick}></input>
             {todo.name}
 
             </label>
