c95ecd9cd4bbe41c09535b959fabc96b80dfdaa9
LinKaiRu <10746041@ntub.edu.tw>
2022-07-19 14:47:03 +0800

    todos,useState

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import React,{ useState } from 'react';
+import TodoList from './TodoList';
+
+function App() {
+  const [todos,setTodos] = useState([{id:1,name:'Todo 1',complete:false}])
+  return (
+    <>
+      <TodoList todos={todos}/>
+      <input type="text" />
+      <button>Add Todo</button>
+      <button>Clear Completed Todos</button>
+      <div>0 left to do</div>
+    </>
+  )
+}
+
+export default App;
